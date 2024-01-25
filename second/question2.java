c752fdffbb5eb7e2ae443d3e839756e73b0db37f
g2Min <jimin9320@gmail.com>
2024-01-25 17:44:36 +0900

    정렬

---
+++ second/question2.java
@@ -0,0 +1,21 @@
+package second;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class question2 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        int[] arr = new int[4];
+        for(int i=0; i<4; i++){
+            arr[i] = Integer.parseInt(str.nextToken());
+        }
+        // arr[0] arr[1] arr[2] arr[3]
+
+    }
+}
---
+++ second/question6.java
@@ -0,0 +1,25 @@
+package second;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class question6 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        int R = Integer.parseInt(str.nextToken()); // 빨간색 타일
+        int B = Integer.parseInt(str.nextToken()); // 갈색 타일
+        int[] arr= new int[2];
+        for(int i=3; i*2<=R-2; i++){
+
+        }
+        System.out.println(arr[0] + " " + arr[1]);
+    }
+}
+
+// 가장자리는 빨간색 그 안에는 갈색
+// 첫과 마지막 줄은 모두 빨간색
+
+// j<=i-2, j*2<=
