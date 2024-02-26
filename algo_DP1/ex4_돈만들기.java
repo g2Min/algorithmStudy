5a2660e8d59b66d0b2ae3a6c7013aefb0db3fb74
g2Min <jimin9320@gmail.com>
2024-02-26 23:56:56 +0900

    day17_dp1

--- algo_DP1/ex4_돈만들기.java
+++ algo_DP1/ex4_돈만들기.java
@@ -3,6 +3,7 @@ package algo_DP1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class ex4_돈만들기 {
@@ -16,25 +17,23 @@ public class ex4_돈만들기 {
         for(int i=0; i<x; i++){
             arr[i] = Integer.parseInt(str2.nextToken());
         }
+//        Arrays.sort(arr); // 오름차순 정렬
         int[] dp = new int[n+1];
-        for(int j=0; j<x; j++){
-            dp[arr[j]] = 1;
+        int MIN = Integer.MAX_VALUE; // 가장 작은 값을 저장할 변수
+
+        for(int j=0; j<x; j++){ //동전의 종류들은 모두 최솟값을 가진다.
+            dp[arr[j]]=1;
         }
+        int j=0;
         for(int i=1; i<=n; i++){
-            for(int j=0; j<x; j++){
-                if(i-arr[j]>=0 && dp[i]!=0){
-                    dp[i] = Math.min(dp[i], dp[i-arr[j]]);
-                }else{
-                    continue;
+            while(j<x){
+                if(i-arr[j]>=0){
+                    dp[i] = dp[i-arr[j]];
                 }
             }
-        }
-        if(dp[n]==0){
-            System.out.println(-1);
-        }else{
-            System.out.println(dp[n]);
         }
     }
 }
 
-// dp(n) = 1+ dp(n-1), 5+dp(n-5) 이런식으로 해서 최솟값 찾기
+// dp(n) = 1+dp(n-1) || 5+dp(n-5) || dp(n-7) 이중에서 최솟값 찾기
+// 모두 -1로 초기화해놓고 0인 index만 0으로 둬서 -1의
