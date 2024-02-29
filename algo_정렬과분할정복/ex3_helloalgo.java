a01f7c4eee965f1aaa83953b90f93117e7642edd
g2Min <jimin9320@gmail.com>
2024-03-01 01:51:40 +0900

    정렬과분할정복

---
+++ algo_정렬과분할정복/ex3_helloalgo.java
@@ -0,0 +1,14 @@
+package algo_정렬과분할정복;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex3_helloalgo {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
+
+// 피보나치=> i = (i-1) + (i-2)
