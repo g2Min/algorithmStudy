71b834248f5ddfff7c13cd097bda9d12515052d4
g2Min <jimin9320@gmail.com>
2024-04-12 22:12:11 +0900

    중복사용과 중복없이의 차이는 큼

--- src/algo_DFS1/q4_N과M1_re.java
+++ src/algo_DFS1/q4_N과M1_re.java
@@ -3,41 +3,41 @@ package algo_DFS1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class q3_모든순열_re {
-
-    static int N;
+public class q4_N과M1_re {
+    static int N, M;
     static int[] arr;
-    static boolean[] visited;
+    static boolean[] visit;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine()); //입력받은 숫자
-        arr = new int[N + 1]; // 배열에 넣는 숫자 (1번 index부터) - 안헷갈리게
-        visited = new boolean[N + 1]; // 1~N 까지의 숫자들 방문 여부
-
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(str.nextToken());
+        M = Integer.parseInt(str.nextToken());
+        arr = new int[M + 1]; //M개의 배열로 숫자 넣기
+        visit = new boolean[N + 1]; //N개의 숫자들에 대해 참거짓
         dfs(1);
         System.out.print(sb);
     }
 
-    public static void dfs(int x) {// x: 배열의 위치
-        if (x >= N + 1) {
-            for (int i = 1; i <= N; i++) {
+    public static void dfs(int x) {// 현재 index
+        if (x >= M + 1) {
+            for (int i = 1; i <= M; i++) {
                 sb.append(arr[i]).append(' ');
             }
             sb.append('\n');
             return;
         }
-
         for (int i = 1; i <= N; i++) {
-            if (visited[i]) continue;
+            if (visit[i]) continue;
             arr[x] = i;
-            visited[i] = true;
+            visit[i] = true;
             dfs(x + 1);
             arr[x] = 0;
-            visited[i] = false;
+            visit[i] = false;
         }
     }
-}
 
+}
