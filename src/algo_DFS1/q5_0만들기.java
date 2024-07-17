5a273762137a4e193b957c1864d61792a425c13e
g2Min <jimin9320@gmail.com>
2024-07-17 23:52:59 +0900

    feat

---
+++ src/algo_DFS1/q5_0만들기.java
@@ -0,0 +1,20 @@
+package algo_DFS1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class q5_0만들기 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());//테스트 케이스 개수
+        for(int i=0; i<T; i++){
+            int num = Integer.parseInt(br.readLine());
+            //dfs(0, num: 한계치)
+        }
+    }
+    public static void dfs(int x, int num){
+
+    }
+}
