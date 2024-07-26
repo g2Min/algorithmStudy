3b0027de0d548584cbbe4bc8abe132a310af037e
g2Min <jimin9320@gmail.com>
2024-07-26 15:43:35 +0900

    전화번호 목록 solved hashmap 이용

--- src/프로그래머스/전화번호_목록.java
+++ src/프로그래머스/전화번호_목록.java
@@ -19,12 +19,18 @@ public class 전화번호_목록 {
 
     }
     public static boolean solution(String[] phone_book){
-        boolean answer = true;
         HashMap<String, Integer> map = new HashMap<>();
         for(int i=0; i<phone_book.length; i++){
-            map.put(phone_book[i])
+            map.put(phone_book[i], i);
         }
 
-        return answer;
+        for(int i=0; i<phone_book.length; i++){
+            for(int j=0; j<phone_book[i].length(); j++){
+                if(map.containsKey(phone_book[i].substring(0, j)))
+                    return false;
+            }
+        }
+
+        return true;
     }
 }
