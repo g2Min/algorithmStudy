6061ee3cc5e7ef0db9c9563380f6d279ff00c82c
g2Min <jimin9320@gmail.com>
2024-08-31 23:36:51 +0900

    점프_점프 solved

--- src/baekjoon/점프_점프.java
+++ src/baekjoon/점프_점프.java
@@ -1,2 +1,36 @@
-package baekjoon;public class 점프_점프 {
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 점프_점프 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n+1];
+        long[] dp = new long[1101];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = Integer.MAX_VALUE;
+        }
+        dp[1] = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dp[i] >= Integer.MAX_VALUE) continue;
+            for (int j = 1; j <= arr[i]; j++) {
+                dp[i+j] = Math.min(dp[i+j], dp[i]+1);
+            }
+        }
+        if (dp[n] >= Integer.MAX_VALUE) {
+            System.out.print(-1);
+        } else {
+            System.out.print(dp[n]);
+        }
+    }
+
 }
+
+/* 1 2 3 4 5
+* */
