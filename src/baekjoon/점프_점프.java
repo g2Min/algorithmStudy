d92a9f91dea3c98689d9be133b7009c54aecb860
g2Min <jimin9320@gmail.com>
2024-09-01 23:50:26 +0900

    점프 점프 solved

--- src/baekjoon/점프_점프.java
+++ src/baekjoon/점프_점프.java
@@ -3,31 +3,41 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class 점프_점프 {
+    public static int T, N;
+    public static int[] arr = new int[1001];
+    public static int[] dp = new int[1001];
+    public static int answer = 0;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n+1];
-        long[] dp = new long[1101];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
+
+        N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            dp[i] = Integer.MAX_VALUE;
         }
-        dp[1] = 0;
-        for (int i = 1; i <= n; i++) {
-            if (dp[i] >= Integer.MAX_VALUE) continue;
-            for (int j = 1; j <= arr[i]; j++) {
-                dp[i+j] = Math.min(dp[i+j], dp[i]+1);
+
+        Arrays.fill(dp, 1001);
+        dp[0] = 0;
+        for(int i=0; i<N;i++) {
+            int step = arr[i];
+            for(int j=i + step; j>i;j--) {
+                if( j < N) {
+                    dp[j] = Math.min(dp[j], dp[i] + 1);
+                }
             }
         }
-        if (dp[n] >= Integer.MAX_VALUE) {
-            System.out.print(-1);
-        } else {
-            System.out.print(dp[n]);
+
+        if(dp[N-1] == 1001) {
+            System.out.println(-1);
+        }else {
+            System.out.println(dp[N-1]);
         }
+
     }
 
 }
