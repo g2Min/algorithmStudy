e0dc29884355316c1f7bb0439e9389d721f9cb1f
g2Min <jimin9320@gmail.com>
2024-09-22 23:49:27 +0900

    달리기 경주 solved

--- src/프로그래머스/달리기_경주.java
+++ src/프로그래머스/달리기_경주.java
@@ -1,17 +1,38 @@
 package 프로그래머스;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class 달리기_경주 {
-    public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.inn))
+class 달리기_경주 {
+    public int findRunner(String[] players, String callName) {
+        int idx = 0;
+
+        for (int i = 0; i < players.length; i++) {
+            if (callName.equals(players[i])) {
+                idx = i;
+            }
+        }
+
+        return idx;
+    }
+
+    public void swapRunner(String[] players, int idx) {
+        String temp = players[idx - 1];
+        players[idx - 1] = players[idx];
+        players[idx] = temp;
+    }
+
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = new String[players.length];
+
+        System.arraycopy(players, 0, answer, 0, players.length);
+
+        // calling의 배열 길이만큼 순회
+        for (int i = 0; i < callings.length; i++) {
+            int idx = findRunner(answer, callings[i]);
+            swapRunner(answer, idx);
+        }
+
+        return answer;
     }
 
 }
-
-/* 자기 바로 앞에있는 선수 한명만 추월함, swap
-* */
