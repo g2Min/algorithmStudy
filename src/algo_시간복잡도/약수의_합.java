480e2f8b6c262bdb22e562eb5d3d94c1ccbbd96a
g2Min <jimin9320@gmail.com>
2024-09-30 22:32:46 +0900

    약수의 합 - 불필요한 계산 없이

--- src/algo_시간복잡도/약수의_합.java
+++ src/algo_시간복잡도/약수의_합.java
@@ -1,2 +1,25 @@
-package algo_시간복잡도;public class 약수의_합 {
+package algo_시간복잡도;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 약수의_합 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+
+        long ans = 0;
+        for(long i=1; i*i<=N; i++){
+            if(N%i == 0){
+                if(i*i != N){
+                    ans += i;
+                    ans += N/i;
+                }else{
+                    ans += i;
+                }
+            }
+        }
+        System.out.print(ans);
+    }
 }
