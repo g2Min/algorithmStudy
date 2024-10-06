f53a13e45cc9712ddb1318ff72ed52a1314c275b
g2Min <jimin9320@gmail.com>
2024-10-06 23:33:00 +0900

    dfs 부속함수

--- src/algo_DFS2/NQUeen.java
+++ src/algo_DFS2/NQUeen.java
@@ -5,35 +5,55 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class NQUeen {
-    static int[][] arr;
-    static int N;
-    static int ans = 0;
-    public static void main(String[] args)throws IOException{
+    public static int[] arr;
+    public static int N;
+    public static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
-        dfs(1);
-        System.out.print(ans);
+        arr = new int[N];
+
+        nQueen(0);
+        System.out.println(count);
+
     }
-    static void dfs(int x){
-        if(x==N){
-            ans ++;
+
+    public static void nQueen(int depth) {
+        // 모든 원소를 다 채운 상태면 count 증가 및 return
+        if (depth == N) {
+            count++;
             return;
         }
 
-        for(int i=0; i<N; i++){
-            boolean pos = true;
-            for(int j=0; j<x; j++){
-                if(i == arr[j] || j - arr[j] == x-i || j + arr[j] == x+i){
-                    pos = false;
-                }
-            }
-
-            if(pos){
-                arr[x] = i;
-                dfs(x+1);
-                arr[x] = 0;
+        for (int i = 0; i < N; i++) {
+            arr[depth] = i;
+            // 놓을 수 있는 위치일 경우 재귀호출
+            if (Possibility(depth)) {
+                nQueen(depth + 1);
             }
         }
+
+    }
+
+    public static boolean Possibility(int col) {
+
+        for (int i = 0; i < col; i++) {
+            // 해당 열의 행과 i열의 행이 일치할경우 (같은 행에 존재할 경우)
+            if (arr[col] == arr[i]) {
+                return false;
+            }
+
+            /*
+             * 대각선상에 놓여있는 경우
+             * (열의 차와 행의 차가 같을 경우가 대각선에 놓여있는 경우다)
+             */
+            else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
