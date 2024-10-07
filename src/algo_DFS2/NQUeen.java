b820f883e4167f4faf716385f351f64b6f214a56
g2Min <jimin9320@gmail.com>
2024-10-07 15:31:56 +0900

    nQueen 다른 접근

--- src/algo_DFS2/NQUeen.java
+++ src/algo_DFS2/NQUeen.java
@@ -5,55 +5,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class NQUeen {
-    public static int[] arr;
-    public static int N;
-    public static int count = 0;
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-
-        nQueen(0);
-        System.out.println(count);
-
-    }
-
-    public static void nQueen(int depth) {
-        // 모든 원소를 다 채운 상태면 count 증가 및 return
-        if (depth == N) {
-            count++;
-            return;
+        static int N;
+        static int[] arr;
+        static int ans;
+        public static void main(String[] args) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            N = Integer.parseInt(br.readLine());
+            arr = new int[N+1];
+            dfs(1);
+            System.out.print(ans);
         }
-
-        for (int i = 0; i < N; i++) {
-            arr[depth] = i;
-            // 놓을 수 있는 위치일 경우 재귀호출
-            if (Possibility(depth)) {
-                nQueen(depth + 1);
-            }
-        }
-
-    }
-
-    public static boolean Possibility(int col) {
-
-        for (int i = 0; i < col; i++) {
-            // 해당 열의 행과 i열의 행이 일치할경우 (같은 행에 존재할 경우)
-            if (arr[col] == arr[i]) {
-                return false;
+        public static void dfs(int x){
+            if(x == N+1){
+                ans ++;
+                return;
             }
 
-            /*
-             * 대각선상에 놓여있는 경우
-             * (열의 차와 행의 차가 같을 경우가 대각선에 놓여있는 경우다)
-             */
-            else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
-                return false;
+            for(int i=1; i<=N; i++){
+                boolean pos = true;
+                for(int j=1; j<x; j++){ //(x, i) vs (j, arr[j])
+                    if(i == arr[j] || Math.abs(j-x) == Math.abs(arr[j]-i)){
+                        pos = false;
+                    }
+                }
+                if(pos){
+                    arr[x] = i;
+                    dfs(x+1);
+                    arr[x] = 0;
+                }
             }
         }
-
-        return true;
-    }
 }
