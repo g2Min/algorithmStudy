8638b755f0892785279a65f7fe2d1cee50380b3c
g2Min <jimin9320@gmail.com>
2024-11-02 00:59:49 +0900

    파티 solved

--- src/baekjoon/다익스트라/파티.java
+++ src/baekjoon/다익스트라/파티.java
@@ -3,10 +3,8 @@ package baekjoon.다익스트라;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class 파티 {
     static class Node{
@@ -37,31 +35,43 @@ public class 파티 {
             int w = Integer.parseInt(st.nextToken());
             graph.get(u).add(new Node(v, w));
         }
-        goTo = new int[N+1];
-        goHome = new int[N+1];
-        int[] res = new int[N+1];
-        goTo[X] = goHome[X] = 0;
 
+        goTo = new int[N+1];
         for(int i=1; i<=N; i++){
-            int[] arr = dijkstra(i, goTo);
+            goTo[i] = dijkstra(i)[X];
         }
 
+        goHome = dijkstra(X);
+
+        int maxTime = 0;
+        for(int i=1; i<=N; i++){
+            if(goTo[i] != Integer.MAX_VALUE && goHome[i] != Integer.MAX_VALUE){
+                maxTime = Math.max(maxTime, goTo[i] + goHome[i]);
+            }
+        }
+        System.out.print(maxTime);
     }
-    static int[] dijkstra(int start, int[] arr){
+    static int[] dijkstra(int start){
         PriorityQueue<Node> pq = new PriorityQueue<>((n1, n2) -> n1.cost-n2.cost);
-        pq.add(new Node(X, 0));
+        int[] dist = new int[N+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+        pq.add(new Node(start, 0));
+
         while(!pq.isEmpty()){
             Node current = pq.poll();
             int currentIdx = current.idx;
             int currentCost = current.cost;
-            if(currentCost > arr[currentIdx]) continue;
+
+            if(currentCost > dist[currentIdx]) continue;
             for(Node neighbor: graph.get(currentIdx)){
-                int newDist = neighbor.cost + arr[currentIdx];
-                if(newDist < arr[neighbor.idx]){
-                    arr[neighbor.idx] = newDist;
+                int newDist = neighbor.cost + dist[currentIdx];
+                if(newDist < dist[neighbor.idx]){
+                    dist[neighbor.idx] = newDist;
+                    pq.add(new Node(neighbor.idx, newDist));
                 }
             }
         }
-        return arr;
+        return dist;
     }
 }
